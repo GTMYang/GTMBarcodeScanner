aa5b408f2830f398e1310ea430152bc0d43ee01d
GTMYang <289135816@qq.com>
2019-03-13 10:25:39 +0800

    ...

--- GTMBarcodeScanner/GTMBarcodeScanner.podspec
+++ GTMBarcodeScanner/GTMBarcodeScanner.podspec
@@ -2,7 +2,7 @@
 Pod::Spec.new do |s|
 
   s.name         = "GTMBarcodeScanner"
-  s.version      = "0.11"
+  s.version      = "0.12"
   s.summary      = "Swift 实现的条码扫描组件库"
   s.swift_version= "4.2"
 
--- GTMBarcodeScanner/GTMBarcodeScanner/Classes/ScannerSound.swift
+++ GTMBarcodeScanner/GTMBarcodeScanner/Classes/ScannerSound.swift
@@ -12,15 +12,15 @@ import AudioToolbox
 class ScannerSound {
     
     static func defSound() {
-        //声明一个系统声音标示类型声音变量
-        var _soundId: SystemSoundID = 0
-        //获取沙箱目录中文件所在的路径
-        let path = Bundle.mine.path(forResource: "qrcode_found", ofType: "wav")
-        //将字符串路径转换为网址路径
-        let soundUrl = URL(fileURLWithPath: path!)
-        
-        AudioServicesCreateSystemSoundID(soundUrl as CFURL, &_soundId)
-        AudioServicesPlaySystemSound(_soundId)
+//        //声明一个系统声音标示类型声音变量
+//        var _soundId: SystemSoundID = 0
+//        //获取沙箱目录中文件所在的路径
+//        let path = Bundle.mine.path(forResource: "qrcode_found", ofType: "wav")
+//        //将字符串路径转换为网址路径
+//        let soundUrl = URL(fileURLWithPath: path!)
+//
+//        AudioServicesCreateSystemSoundID(soundUrl as CFURL, &_soundId)
+//        AudioServicesPlaySystemSound(_soundId)
     }
     
     static func sound(forResource resource: String, ofType type: String) {
